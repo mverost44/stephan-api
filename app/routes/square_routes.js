88c1fc2ce50b3b08f1d98aa20d669f9365c1a026
mverost <mverost4422@gmail.com>
2019-06-06 11:39:12 -0400

    added mandrill after transaction

--- app/routes/square_routes.js
+++ app/routes/square_routes.js
@@ -4,21 +4,33 @@ const router = express.Router()
 
 const keygen = require('keygenerator')
 
+require('dotenv').config()
+
+const email = require('powerdrill')(process.env.MANDRILL_KEY)
+
 const SquareConnect = require('square-connect')
 
 const defaultClient = SquareConnect.ApiClient.instance
 
-require('dotenv').config()
-
 // Configure OAuth2 access token for authorization: oauth2
 const oauth2 = defaultClient.authentications['oauth2']
-oauth2.accessToken = process.env.SQUARE_TOKEN
+oauth2.accessToken = process.env.SANDBOX_TOKEN
 
 const apiInstance = new SquareConnect.CustomersApi()
 const transactionInstance = new SquareConnect.TransactionsApi()
 
+let customerEmail
+
+const inviteEmail = function (customerEmail) {
+  const message = email('facebook-invite')
+
+  message
+    .to(customerEmail)
+    .send()
+}
+
 const transactionPromise = function (data, customerId) {
-  const locationId = process.env.LOCATION
+  const locationId = process.env.SANDBOX_LOCATION
   const body = {
     idempotency_key: keygen.transaction_id(),
     customer_id: customerId,
@@ -33,9 +45,11 @@ const transactionPromise = function (data, customerId) {
 }
 
 router.post('/customer', (req, res, next) => {
+  customerEmail = req.body.customer.email
+
   const body = {
     given_name: req.body.customer.name,
-    email_address: req.body.customer.email
+    email_address: customerEmail
   } // CreateCustomerRequest | An object containing the fields to POST for the request.  See the corresponding object definition for field details.
   apiInstance.createCustomer(body)
     .then(data => {
@@ -53,6 +67,9 @@ router.post('/customer-card', (req, res, next) => {
       return transactionPromise(data, customerId)
     })
     .then(data => {
+      if (data.transaction) {
+        inviteEmail(customerEmail)
+      }
       return res.status(201).json({ data })
     })
     .catch(next)
